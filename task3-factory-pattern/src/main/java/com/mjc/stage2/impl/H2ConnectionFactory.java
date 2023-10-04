0710a111a71ff40294c7a0116e317a1ae9fcff6d
Mikita <vishnevkiy.nikita@gmail.com>
2023-10-05 02:37:29 +0300

    1

--- task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
+++ task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
@@ -1,6 +1,35 @@
 package com.mjc.stage2.impl;
 
-public class H2ConnectionFactory {
-    // Write your code here!
+import com.mjc.stage2.ConnectionFactory;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class H2ConnectionFactory implements ConnectionFactory {
+    @Override
+    public Connection createConnection(){
+        Properties properties = new Properties();
+        try {
+            properties.load(H2ConnectionFactory.class.getClassLoader().getResourceAsStream("h2database.properties"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            Class.forName(properties.getProperty("jdbc_driver"));
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        String url = properties.getProperty("db_url");
+        String user = properties.getProperty("user");
+        String password = properties.getProperty("password");
+        try {
+            return DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
 
