873c8b251d2bce9f700af1212ad149527e57fae4
Mikita <vishnevkiy.nikita@gmail.com>
2023-10-05 02:59:25 +0300

    4

--- task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
+++ task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
@@ -5,7 +5,7 @@ public class ThreadSafeSingleton {
 
     private ThreadSafeSingleton(){}
 
-    public static ThreadSafeSingleton getInstance(){
+    public synchronized static ThreadSafeSingleton getInstance(){
         return instance;
     }
 }
